e8af7c5123435933bf45ec9404cf6a8296d34ca1
Tzu-Ching Yen <thomson.yen@mail.utoronto.ca>
2026-02-06 09:47:47 -0500

    Initialize judge folder setup

---
+++ judge/project_configs.sh
@@ -0,0 +1,12 @@
+# Setup Langfuse
+export LANGFUSE_PUBLIC_KEY=""
+export LANGFUSE_SECRET_KEY=""
+export LANGFUSE_BASE_URL=""
+export OPENROUTER_API_KEY=""
+
+# Setup DB
+export DATABASE_URL=""
+export OLD_DATABASE_URL=""
+
+# Setup Storage
+export SCRATCH_PATH=""
