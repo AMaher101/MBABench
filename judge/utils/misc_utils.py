f6394a95e80f8169fe9414649ee1a2e659a94134
Tzu-Ching Yen <thomson.yen@mail.utoronto.ca>
2026-02-11 16:59:19 -0500

    Add pathing helper

--- judge/utils/misc_utils.py
+++ judge/utils/misc_utils.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import yaml
 
@@ -6,9 +7,20 @@ _CONFIG_PATH = os.path.join(os.path.dirname(__file__), "..", "project_configs.ya
 _CONFIG_PATH = os.path.abspath(_CONFIG_PATH)
 
 
+def relative_path_from_project_root(path) -> str:
+    """Convert a path to be relative from the project root directory."""
+    project_root = Path(__file__).parent.parent.resolve()
+    path = Path(path)
+
+    # interpret .. or . as relative to project root and resolve to absolute path
+    absolute_path = (project_root / path).resolve()
+    return absolute_path
+
+
 def load_project_configs():
     """Load project_configs.yaml and set non-null values as environment variables."""
     print(f"Loading project configs from {_CONFIG_PATH}...")
+    loaded_configs = {}
     with open(_CONFIG_PATH) as f:
         config = yaml.safe_load(f)
 
@@ -19,3 +31,6 @@ def load_project_configs():
             if value is not None:
                 print(f"Setting env var {key.upper()} = {value}")
                 os.environ[key.upper()] = str(value)
+                loaded_configs[key.upper()] = value
+
+    return loaded_configs
